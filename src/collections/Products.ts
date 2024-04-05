c3f08dc27fd3fe24299463b767a404ae1abb2507
k_czech <kamczeh@gmail.com>
2024-04-05 10:50:20 +0200

    initial commit

---
+++ src/collections/Products.ts
@@ -0,0 +1,42 @@
+import { CollectionConfig } from 'payload/types'
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  fields: [
+   {
+     name: 'name',
+     type: 'text',
+     localized: true,
+     required: true,
+     unique: true
+   },
+   {
+    name: 'slug',
+    type: 'text',
+    localized: true,
+    required: true,
+    unique: true
+   },
+   {
+    name: 'description',
+    type: 'richText',
+    localized: true,
+    required: true
+   },
+   {
+    name: 'image',
+    type: 'upload',
+    relationTo: 'media',
+    required: true
+   },
+   {
+    name: 'price',
+    type: 'number',
+    required: true
+   }
+  ],
+  graphQL: {
+    pluralName: 'products',
+    singularName: 'product',
+}
+}
